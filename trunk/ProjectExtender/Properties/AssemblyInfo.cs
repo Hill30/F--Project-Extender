9eaea18e36395bf2a51bd17facdfc79f08e3fdd0
SND\svlasova_cp <SND\svlasova_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2010-06-22 07:56:58 +0000

    v0.9.3.0 release
    git-svn-id: https://fsprojectextender.svn.codeplex.com/svn@47653 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

--- trunk/ProjectExtender/Properties/AssemblyInfo.cs
+++ trunk/ProjectExtender/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.9.2.0")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
 
 //[assembly: InternalsVisibleTo("ProjectExtender_IntegrationTestProject")]
 //[assembly: InternalsVisibleTo("ProjectExtender_UnitTestProject")]
